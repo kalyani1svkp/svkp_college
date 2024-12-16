8ce4010f1783d7280d5b21cbe8c0b9f82132ebf7
HI <HI@192.168.1.36>
2024-12-16 00:31:27 -0800

    commit5

---
+++ svkp_college/src/lambdaexpressiondemo/WithOutLambdaExp.java
@@ -0,0 +1,21 @@
+package lambdaexpressiondemo;
+//demo on withoutlambdaexp
+interface Drawable //functional interface
+{
+	public void draw(); 
+}
+class Test implements Drawable
+{
+	int width=10;
+	@Override
+	public void draw() {
+		System.out.println("Drawing "+width);	
+	}
+}
+public class WithOutLambdaExp {
+
+	public static void main(String[] args) {
+		Drawable d=new Test();
+		d.draw();
+	}
+}
